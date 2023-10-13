7adfcc556b3127711c47a65cb5c55868b843207c
ichi23B <aichi_fumiya@avilen.co.jp>
2023-10-13 15:48:36 +0900

    add:spec5: idの追加

--- dictionary.py
+++ dictionary.py
@@ -13,33 +13,34 @@ class Dictionary():
             dict_path (Path): 辞書ファイルのパスオブジェクト
         """
         self.dict_path = dict_path
-        self.words = []
+        self.words = {}
 
 
     def read_dict_file(self):
-        """辞書ファイルを読み込み単語リストを作成する
+        """辞書ファイルを読み込み、keyがid・valueが単語の辞書を作成する
 
         Raises:
             FileNotFoundError: 辞書ファイルが見つからない
 
         Returns:
-            list[str]: 単語リスト
+            {id: 単語}: id・単語辞書
         """
-        self.words = [] # 単語リスト初期化
+        self.words = {} # id・単語辞書初期化
         if self.dict_path.exists():
             with self.dict_path.open(encoding='utf-8') as f:
-                word = f.read()
-                self.words.append(word)
+                word_row = f.readlines()
+                for id, word in enumerate(word_row):
+                    self.words[id] = word.rstrip()
             return self.words    
         else:
             raise FileNotFoundError()
 
 
     def print_words(self):
-        """単語リストを表示
+        """id・単語辞書を表示
         """
-        for word in self.words:
-            print(word)
+        for id, word in self.words.items():
+            print(f'{id}: {word}')
 
 
 if __name__ == '__main__':
